cc07abd7cfe332310b27b5cbf05af45c7098b075
DidierFontaine <71550187+DidierFontaine@users.noreply.github.com>
2020-11-12 13:22:23 +0100

    Add files via upload

---
+++ evenOddReporter.js
@@ -0,0 +1,13 @@
+for (let index = 0; index <=20; index++) {
+    console.log(index);
+    if (index % 2){
+    console.log("Jij bent oneven")
+    } else {
+    console.log("Jij bent even")
+    }
+}
+
+const array = ['q', 'w', 'e'];
+array.forEach((item) => {
+    console.log(item);
+})
---
+++ multiplicationTables.js
@@ -0,0 +1,5 @@
+for (let tafel = 1; tafel <= 10; tafel++) {
+    for (let multiplier = 1; multiplier <= 10; multiplier++) {
+        console.log(tafel + ' x '  + multiplier + ' = ' + tafel * multiplier);
+    } 
+}
